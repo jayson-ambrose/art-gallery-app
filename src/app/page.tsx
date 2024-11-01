54e046f2cef7c345d071966ba42f9e156b40ebe5
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-11-01 10:30:08 -0600

    implemented scroll to section from navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <p className="text-4xl">Ambrose Arts</p>
+      <div className="flex cursor-pointer">
+        <Link className="mx-5" href={"#gallery-section"} scroll={true}>Gallery</Link>
+        <Link href={"#contact-section"} scroll={true}>Contact Us</Link>
+      </div>
       <p>Welcome to Ambrose Arts. Feel free to browse our gallery.</p>
-      <p className="text-4xl">Gallery</p>
+      <p className="text-4xl" id='gallery-section'>Gallery</p>
       <div>
         <img className="w-96 h-auto" src="https://cdn.imgchest.com/files/my2pclnb8b7.png"/>
+        <img className="w-96 h-auto" src="https://cdn.imgchest.com/files/my2pclnb8b7.png"/>
+        <img className="w-96 h-auto" src="https://cdn.imgchest.com/files/my2pclnb8b7.png"/>
+        <img className="w-96 h-auto" src="https://cdn.imgchest.com/files/my2pclnb8b7.png"/>
+        <img className="w-96 h-auto" src="https://cdn.imgchest.com/files/my2pclnb8b7.png"/>
+        <img className="w-96 h-auto" src="https://cdn.imgchest.com/files/my2pclnb8b7.png"/>
+        <img className="w-96 h-auto" src="https://cdn.imgchest.com/files/my2pclnb8b7.png"/>
       </div>
-      <div>
-        <p className="text-4xl">Contact Us</p>
+      <p className="text-4xl" id='contact-section'>Contact Us</p>
+      <div className="flex flex-col w-1/2">
+        <p>For more information on any of our pieces, please contact us at ambrosearts@gmail.com</p>
+        <input type="text"/>
+        <input type="text"/>
+        <textarea className='mb-96'/>
       </div>
-      <p>For more information on any of our pieces, please contact us at ambrosearts@gmail.com</p>
     </div>
   );
 }
